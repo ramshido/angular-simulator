06ff6419b9a735ca57c62ce23cbb27d52f1e46b3
Khao Kai <Ramza@gmail.com>
2026-01-31 20:29:57 +0300

    [14](feat): created the following fns: getFormatedText, getRemovedSymbol, getSum; created the following interfaces and types: IUser, IPayer, TypeUploadStatus, TypeTextFormat

---
+++ src/app/training.ts
@@ -0,0 +1,64 @@
+interface IUser {
+	id: number;
+	name: string;
+	lastName: string;
+	age: number;
+	email?: string;
+}
+
+interface IPlayer extends IUser {
+	level: number;
+	strength: string;
+}
+
+type TypeUploadStatus = "loading" | "success" | "error";
+
+let uploadingStatus: TypeUploadStatus;
+
+type TypeTextFormat = 'uppercase' | 'lowercase' | 'capitalize';
+
+let textFormat: TypeTextFormat;
+
+function getSum(a: number, b: number): number {
+	return a + b;
+}
+
+console.log(getSum(5, 5));
+
+const getFormatedText = (text: string, format: TypeTextFormat): string => {
+	switch (format) {
+		case "uppercase":
+			return text.toUpperCase();
+		case "lowercase":
+			return text.toLowerCase();
+		case "capitalize":
+			return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+		default:
+			return text;
+	}
+}
+
+getFormatedText("Salam", "uppercase");
+
+const getRemovedSymbol: (text: string, symbol: string) => string = (text: string, symbol: string): string => {
+	return text.replaceAll(symbol, "");
+}
+
+getRemovedSymbol("Vlad", "V");
+
+const users: IUser[] = [
+	{
+		id: 1,
+		name: 'Bip',
+		age: 21,
+		lastName: 'Bop'
+	},
+	{
+		id: 2,
+		name: 'Claus',
+		age: 18,
+		lastName: 'Santa'
+	}
+];
+
+const filteredUsers: IUser[] = users.filter((user: IUser) => user.id !== 2);
