9973812320f0519b5439a4a2ab205ff07f7f2732
Khao Kai <Ramza@gmail.com>
2026-03-06 14:27:34 +0300

    [17](chore): fixed messages.service.ts by added setTimeout

--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -7,14 +7,6 @@ export class MessagesService {
 
 	messages: IMessage[] = [];
 
-	constructor() {
-		setInterval(() => {
-			if (this.messages.length) {
-				this.messages = this.messages.filter((message: IMessage, index: number) => index !== this.messages.length - 1);
-			}
-		}, 5000);
-	}
-
 	addMessage(message: { type: MessageType, message: string }): void {
 		this.messages = [
 			{
@@ -23,6 +15,12 @@ export class MessagesService {
 			},
 			...this.messages,
 		];
+
+		setTimeout(() => {
+			if (this.messages.length) {
+				this.messages = this.messages.filter((message: IMessage, index: number) => index !== this.messages.length - 1);
+			}
+		}, 5000);
 	}
 
 	closeMessage(id: number): void {
